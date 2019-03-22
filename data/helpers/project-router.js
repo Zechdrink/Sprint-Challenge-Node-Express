d1d3f5ac0ef76785cab47f7ca67a787399ba9115
Zech Drinkhall <zechdrink@aol.com>
2019-03-22 10:22:55 -0700

    finished the get, post, delete and put for project

--- data/helpers/project-router.js
+++ data/helpers/project-router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const projects = await Projects.get(req.query);
+        const projects = await Projects.get(req.params.query);
         res.status(200).json(projects);
     } catch {
         res.status(500).json(
@@ -14,6 +14,62 @@ router.get('/', async (req, res) => {
         )
     }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await Projects.getProjectActions(req.params.id)
+
+        if(project){
+            res.status(200).json(project)
+        } else {
+            res.status(404).json('project not found')
+        }
+    }
+
+    catch(error) {
+            res.status(500).json('error retrieving the project')
+    }
+})
+
+
+router.post('/', async(req, res) => {
+    try {
+        const project = await Projects.insert(req.body);
+        res.status(201).json(project);
+    } catch {
+    res.status(500).json('error adding the project')
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const count = await Projects.remove(req.params.id);
+        if(count > 0){
+            res.status(200).json("The project aborted")
+        } else {
+            res.status(401).json("The project is MIA")
+        }
+    } catch {
+        res.status(500).json("Error aborting the project")
+    }
+})
+
+router.put('/:id', async(req, res) => {
+    try {
+        const project = await Projects.update(req.params.id, req.body);
+
+        if(project){
+            res.status(200).json(project)
+        } else {
+            res.status(404).json('The project is nowhere')
+        }
+    } catch {
+        res.status(500).json('error updating the project')
+    }
+})
+
+
+
 
 
 
